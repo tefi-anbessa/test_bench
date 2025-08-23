483c2b9ec8567c06f2aeb54eb133b71415e7a663
tefi-anbessa <frankrobinson99@hotmail.com>
2025-08-23 07:44:16 +0700

    Refactor role policy to incorporate current project and user context

--- app/policies/role_policy.rb
+++ app/policies/role_policy.rb
@@ -4,38 +4,92 @@ class RolePolicy < ApplicationPolicy
   end
 
   class Scope < ApplicationPolicy::Scope
-    attr_reader :project
-
     def resolve
-      if user&.is_app_owner? || user&.has_role?(:admin)
-        scope.all
-      elsif current_project.present?
-        # Show roles for the current project
-        scope.where(resource: current_project)
-      else
-        scope.none
+      # App owners and admins see all roles
+      return scope.all if user&.is_app_owner? || user&.has_role?(:admin)
+      
+      # For project context, show roles for that project
+      if current_project.present?
+        # Project owners and team members can see roles for their project
+        if user.present? && (user.has_role?(:project_owner, current_project) || 
+                            user.has_role?(:team_member, current_project))
+          return scope.where(resource: current_project)
+        end
       end
+      
+      # Regular users see no roles by default
+      scope.none
     end
   end
 
-  # Anyone can view the roles index if a project is selected
+  # If current project is set, all users except regular users can view roles
+  # If no current project, only app owners and admins can view roles
   def index?
-    current_project.present?
+    if current_project.present?
+      user.present? && (user.is_app_owner? || user.has_role?(:admin) || 
+                       user.has_role?(:project_owner, current_project) || 
+                       user.has_role?(:team_member, current_project))
+    else
+      user.present? && (user.is_app_owner? || user.has_role?(:admin))
+    end
   end
 
-  # Only app owners can view the new role form
+  # For global roles (no resource), only app owners and admins can access new form
+  # For resource roles, access is controlled via the resource's show view
   def new?
-    user.present? && user.is_app_owner?
+    return false unless user.present?
+    
+    # For resource roles, we don't use the new action - access is controlled by the resource's show view
+    return false if record.is_a?(Class) && record != Role
+    
+    # For global roles, only app owners and admins can access
+    user.is_app_owner? || user.has_role?(:admin)
   end
 
-  # Only app owners can create new roles
+  # Only app owners can create global roles
+  # Admins can create functional roles (except admin/app_owner)
+  # For resource roles, the ability to create is determined by the resource's policy
   def create?
-    user.present? && user.is_app_owner?
+    return false unless user.present?
+    
+    # For resource roles, the resource's policy will control access
+    if record.is_a?(Role) && record.resource.present?
+      return record.resource_policy.new(user_context, record.resource).create_role?(record.name)
+    end
+    
+    # Check if this is a global role creation
+    if record.is_a?(Class) || (record.is_a?(Role) && record.resource.nil?)
+      # Only app owners can create admin or app_owner roles
+      if %w[admin app_owner].include?(record.try(:name).to_s)
+        return user.is_app_owner?
+      end
+      
+      # App owners and admins can create other global roles
+      return user.is_app_owner? || user.has_role?(:admin)
+    end
+    
+    false
   end
 
-  # Only app owners can destroy roles
+  # App owners and admins can destroy any role
+  # Project owners can destroy roles within their projects
   def destroy?
-    user.present? && user.is_app_owner?
+    return false unless user.present?
+    
+    # App owners and admins can delete any role
+    return true if user.is_app_owner? || user.has_role?(:admin)
+    
+    # For project roles, check if the user is a project owner of the resource
+    if record.resource.is_a?(Project)
+      return user.has_role?(:project_owner, record.resource)
+    end
+    
+    # For other resource roles, check if the user is a project owner of the resource's project
+    if record.resource.respond_to?(:project) && record.resource.project.present?
+      return user.has_role?(:project_owner, record.resource.project)
+    end
+    
+    false
   end
 
   private
--- test/policies/role_policy_test.rb
+++ test/policies/role_policy_test.rb
@@ -27,73 +27,147 @@ class RolePolicyTest < ActiveSupport::TestCase
     ApplicationPolicy::UserContext.new(user, project)
   end
 
-  # Index action - requires a project context
-  test 'index requires project context' do
-    # With project context
-    assert RolePolicy.new(user_context(@app_owner, @project), Role).index?
-    assert RolePolicy.new(user_context(@admin, @project), Role).index?
-    assert RolePolicy.new(user_context(@project_owner, @project), Role).index?
-    assert RolePolicy.new(user_context(@team_member, @project), Role).index?
-    assert RolePolicy.new(user_context(@regular_user, @project), Role).index?
+  # Index action - if current project is set, all users except regular users can view roles
+  test 'app owners, admins, project owners and team members can view roles when current project is set' do
+    assert RolePolicy.new(user_context(@app_owner, @project), Role).index?,
+      'App owner should be able to view roles with project context'
+    assert RolePolicy.new(user_context(@admin, @project), Role).index?,
+      'Admin should be able to view roles with project context'
+    assert RolePolicy.new(user_context(@project_owner, @project), Role).index?,
+      'Project owner should be able to view roles with project context'
+    assert RolePolicy.new(user_context(@team_member, @project), Role).index?,
+      'Team member should be able to view roles with project context'
+    refute RolePolicy.new(user_context(@regular_user, @project), Role).index?,
+      'Regular user should not be able to view roles even with project context'
+  end
+
+  # Index action - if no current project is selected, only app owners and admins can view roles
+  test 'only app owners and admins can view roles when no current project' do
+    assert RolePolicy.new(user_context(@app_owner, nil), nil).index?,
+      'App owner should be able to view roles without project context'
+    assert RolePolicy.new(user_context(@admin, nil), nil).index?,
+      'Admin should be able to view roles without project context'
+    refute RolePolicy.new(user_context(@project_owner, nil), nil).index?,
+      'Project owner should not be able to view roles without project context'
+    refute RolePolicy.new(user_context(@team_member, nil), nil).index?,
+      'Team member should not be able to view roles without project context'
+    refute RolePolicy.new(user_context(@regular_user, nil), nil).index?,
+      'Regular user should not be able to view roles without project context'
+  end
+
+  # New action - only app owners and admins can access global role creation
+  test 'new only allows app owners and admins to access global role creation' do
+    # Test global role creation (record is the Role class)
+    assert RolePolicy.new(user_context(@app_owner, nil), Role).new?,
+      'App owner should be able to access new global role form'
+    assert RolePolicy.new(user_context(@admin, nil), Role).new?,
+      'Admin should be able to access new global role form'
+      
+    # Other roles should not have access to global role creation
+    refute RolePolicy.new(user_context(@project_owner, @project), Role).new?,
+      'Project owner should not be able to access new global role form'
+    refute RolePolicy.new(user_context(@team_member, @project), Role).new?,
+      'Team member should not be able to access new global role form'
+    refute RolePolicy.new(user_context(@regular_user, @project), Role).new?,
+      'Regular user should not be able to access new global role form'
+    refute RolePolicy.new(user_context(nil, @project), Role).new?,
+      'Guest user should not be able to access new global role form'
+  end
+
+  test 'create role permissions' do
+    # Test admin/app_owner role creation - only app owner can do this
+    admin_role = Role.new(name: 'admin', resource: nil)
+    app_owner_role = Role.new(name: 'app_owner', resource: nil)
     
-    # Without project context
-    refute RolePolicy.new(user_context(@app_owner), Role).index?
-    refute RolePolicy.new(user_context(nil), Role).index?
+    assert RolePolicy.new(user_context(@app_owner, nil), admin_role).create?,
+      'App owner should be able to create admin role'
+    assert RolePolicy.new(user_context(@app_owner, nil), app_owner_role).create?,
+      'App owner should be able to create app_owner role'
+      
+    refute RolePolicy.new(user_context(@admin, nil), admin_role).create?,
+      'Admin should not be able to create admin role'
+    refute RolePolicy.new(user_context(@admin, nil), app_owner_role).create?,
+      'Admin should not be able to create app_owner role'
+    
+    # Test functional role creation - both app owner and admin can do this
+    functional_role = Role.new(name: 'functional_role', resource: nil)
+    
+    assert RolePolicy.new(user_context(@app_owner, nil), functional_role).create?,
+      'App owner should be able to create functional roles'
+    assert RolePolicy.new(user_context(@admin, nil), functional_role).create?,
+      'Admin should be able to create functional roles'
+    
+    # Other users cannot create any global roles
+    refute RolePolicy.new(user_context(@project_owner, nil), functional_role).create?,
+      'Project owner should not be able to create global roles'
+    refute RolePolicy.new(user_context(@team_member, nil), functional_role).create?,
+      'Team member should not be able to create global roles'
+    refute RolePolicy.new(user_context(@regular_user, nil), functional_role).create?,
+      'Regular user should not be able to create global roles'
+    refute RolePolicy.new(user_context(nil, nil), functional_role).create?,
+      'Guest user should not be able to create global roles'
+      
+    # Note: Resource role creation is handled by the resource's policy
+    # and should be tested in the resource's policy tests
   end
 
-  # New action - only app owners can access
-  test 'new only allows app owners' do
-    assert RolePolicy.new(user_context(@app_owner, @project), Role).new?
-    refute RolePolicy.new(user_context(@admin, @project), Role).new?
-    refute RolePolicy.new(user_context(@project_owner, @project), Role).new?
-    refute RolePolicy.new(user_context(@team_member, @project), Role).new?
-    refute RolePolicy.new(user_context(@regular_user, @project), Role).new?
-    refute RolePolicy.new(user_context(nil, @project), Role).new?
-  end
+  # Destroy action - role deletion permissions
+  test 'destroy permissions' do
+    # Global roles - only app owners and admins can destroy
+    assert RolePolicy.new(user_context(@app_owner, nil), @global_role).destroy?,
+      'App owner should be able to destroy global roles'
+    assert RolePolicy.new(user_context(@admin, nil), @global_role).destroy?,
+      'Admin should be able to destroy global roles'
+    refute RolePolicy.new(user_context(@project_owner, nil), @global_role).destroy?,
+      'Project owner should not be able to destroy global roles'
 
-  # Create action - only app owners can access
-  test 'create only allows app owners' do
-    assert RolePolicy.new(user_context(@app_owner, @project), Role).create?
-    refute RolePolicy.new(user_context(@admin, @project), Role).create?
-    refute RolePolicy.new(user_context(@project_owner, @project), Role).create?
-    refute RolePolicy.new(user_context(@team_member, @project), Role).create?
-    refute RolePolicy.new(user_context(@regular_user, @project), Role).create?
-    refute RolePolicy.new(user_context(nil, @project), Role).create?
-  end
-
-  # Destroy action - only app owners can access
-  test 'destroy only allows app owners' do
-    assert RolePolicy.new(user_context(@app_owner, @project), @global_role).destroy?
-    refute RolePolicy.new(user_context(@admin, @project), @global_role).destroy?
-    refute RolePolicy.new(user_context(@project_owner, @project), @project_role).destroy?
-    refute RolePolicy.new(user_context(@team_member, @project), @project_role).destroy?
-    refute RolePolicy.new(user_context(@regular_user, @project), @project_role).destroy?
-    refute RolePolicy.new(user_context(nil, @project), @project_role).destroy?
+    # Project roles - project owners can destroy roles in their projects
+    assert RolePolicy.new(user_context(@project_owner, @project), @project_role).destroy?,
+      'Project owner should be able to destroy roles in their project'
+    refute RolePolicy.new(user_context(@team_member, @project), @project_role).destroy?,
+      'Team member should not be able to destroy roles in their project'
+    refute RolePolicy.new(user_context(@regular_user, @project), @project_role).destroy?,
+      'Regular user should not be able to destroy project roles'
+    refute RolePolicy.new(user_context(nil, @project), @project_role).destroy?,
+      'Guest user should not be able to destroy project roles'
   end
 
   # Scope filters roles based on user and project
   test 'scope filters roles based on user and project' do
     # App owner sees all roles
     app_owner_scope = RolePolicy::Scope.new(user_context(@app_owner), Role).resolve
-    assert_includes app_owner_scope, @global_role
-    assert_includes app_owner_scope, @project_role
+    assert_includes app_owner_scope, @global_role,
+      'App owner should see global roles'
+    assert_includes app_owner_scope, @project_role,
+      'App owner should see project roles'
 
     # Admin sees all roles
     admin_scope = RolePolicy::Scope.new(user_context(@admin), Role).resolve
-    assert_includes admin_scope, @global_role
-    assert_includes admin_scope, @project_role
+    assert_includes admin_scope, @global_role,
+      'Admin should see global roles'
+    assert_includes admin_scope, @project_role,
+      'Admin should see project roles'
 
-    # Project owner sees project roles
+    # Project owner sees only their project's roles
     project_owner_scope = RolePolicy::Scope.new(user_context(@project_owner, @project), Role).resolve
-    refute_includes project_owner_scope, @global_role
-    assert_includes project_owner_scope, @project_role
+    refute_includes project_owner_scope, @global_role,
+      'Project owner should not see global roles'
+    assert_includes project_owner_scope, @project_role,
+      'Project owner should see roles from their project'
 
-    # Regular user sees project roles
-    regular_user_scope = RolePolicy::Scope.new(user_context(@regular_user, @project), Role).resolve
-    refute_includes regular_user_scope, @global_role
-    assert_includes regular_user_scope, @project_role
+    # Team member sees only their project's roles
+    team_member_scope = RolePolicy::Scope.new(user_context(@team_member, @project), Role).resolve
+    refute_includes team_member_scope, @global_role,
+      'Team member should not see global roles'
+    assert_includes team_member_scope, @project_role,
+      'Team member should see roles from their project'
 
-    # No project context shows no roles
-    assert_empty RolePolicy::Scope.new(user_context(@regular_user), Role).resolve
+    # Regular user without project context sees no roles
+    assert_empty RolePolicy::Scope.new(user_context(@regular_user), Role).resolve,
+      'Regular user without project context should see no roles'
+      
+    # Regular user with project context but no role sees no roles
+    assert_empty RolePolicy::Scope.new(user_context(@regular_user, @project), Role).resolve,
+      'Regular user without project role should see no roles'
   end
 end
